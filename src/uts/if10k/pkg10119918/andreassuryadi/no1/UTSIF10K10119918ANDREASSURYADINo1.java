64b0d64a30e8158a71264822bc23f373507ea9f9
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-12-04 20:38:57 +0700

    UTS-IF10K-10119918-ANDREASSURYADI-no1

---
+++ src/uts/if10k/pkg10119918/andreassuryadi/no1/UTSIF10K10119918ANDREASSURYADINo1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10k.pkg10119918.andreassuryadi.no1;
+
+/**
+ *
+ * @author 
+ * NAMA      : Andreas Suryadi
+ * KELAS     : IF-10K
+ * NIM       : 10119918
+ * Deskripsi Program : Program ini bertujuan untuk menampilkan keterangan 
+ * berdasarkan umur
+ * 
+ */
+
+public class UTSIF10K10119918ANDREASSURYADINo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
